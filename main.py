146442089dc0bbb40e258579d603b3c60af23b60
zhangc <zhangc@educ.college-montreal.qc.ca>
2023-11-20 10:42:26 -0500

    tp3

--- main.py
+++ main.py
@@ -5,59 +5,74 @@ victoryCurrent = 0
 victoryTotal = 0
 lossTotal = 0
 fightTotal = 0
+monsterTotal= 0
 
 def printStatus():
     print("=============================")
-    print("Niveau de vie de l’usager ：" + str(playHp))
+    print("Adversaire:"+ str(monsterTotal))
+    print("Force de l’adversaire: "+str(monsterDmg))
+    print("Niveau de vie de l’usager：" + str(playHp))
     print("Combat " + str(fightTotal)+": "+ str(victoryTotal)+" victoire vs "+ str(lossTotal)+" défaite")
     print("=============================")
 
-while True:
+while playHp <= 0:
     if victoryCurrent >= 3:
-        print("打BOSS...")
-        fightTotal += 1
-        victoryTotal += 1
-        victoryCurrent += 1
-        victoryCurrent = 0
-        print("赢了...")
-        continue
-    else:
-        printStatus()
-        monsterDmg = random.randint(1, 5)
+        print("BOSS...")
+        monsterDmg = random.randint(2,12
         print("Vous tombez face à face avec un adversaire de difficulté:" + str(monsterDmg))
+        monsterTotal += 1
+        printStatus()
+        victoryTotal += 1
+        victoryCurrent = 0
+        if choice == "1":
+            playDmg = random.randint(3, 6)
+            print("Lancer du dé：" + str(playDmg))
+            if monsterDmg < playDmg:
+                print("Dernier combat = gagner")
+                print("")
+                playHp = playHp + monsterDmg
+                fightTotal += 1
+                victoryTotal += 1
+                victoryCurrent += 1
+            else:
+                print("Dernier combat = défaite")
+                print("")
+                playHp = playHp - monsterDmg
+                fightTotal += 1
+                lossTotal += 1
+
+    else:
+        monsterDmg = random.randint(1,5 )
+        print("Vous tombez face à face avec un adversaire de difficulté:" + str(monsterDmg))
+        monsterTotal += 1
+        printStatus()
         choice = input('''Que voulez-vous faire ?
 1- Combattre cet adversaire
 2- Contourner cet adversaire et aller ouvrir une autre porte
 3- Afficher les règles du jeu
 4- Quitter la partie
                         ''')
-
-
         if choice == "1":
-            playDmg = random.randint(3, 6)
+            playDmg = random.randint(1, 6)
             print("Lancer du dé：" + str(playDmg))
             if monsterDmg < playDmg:
                 print("Dernier combat = gagner")
+                print("")
                 playHp = playHp + monsterDmg
                 fightTotal += 1
                 victoryTotal += 1
                 victoryCurrent += 1
             else:
+                print("Dernier combat = défaite")
+                print("")
                 playHp = playHp - monsterDmg
                 fightTotal += 1
                 lossTotal += 1
-                if playHp <= 0:
-                    print("游戏结果")
-                    print("Exiting the program...")
-                    break
+
 
         elif choice == "2":
-            monsterDmg = random.randint(1, 6)
             playHp = playHp - 1
-            if playHp <= 0:
-                print("游戏结果")
-                print("Exiting the program...")
-                break
+
 
         elif choice == "3":
             print("=============================")
@@ -75,3 +90,11 @@ L’usager peut combattre ou éviter chaque adversaire, dans le cas de l’évit
         elif choice == "4":
             print("Merci et au revoir...")
             break
+
+print("La partie est terminée, vous avez vaincu "+str(victoryTotal)+" nombre_victoires monstre(s)")
+print("Exiting the program...")
+
+#if playHp <= 0:
+    #print(La partie est terminée, vous avez vaincu "+str(victoryTotal)+" nombre_victoires monstre(s)")
+    #print("Exiting the program...")
+    #break
